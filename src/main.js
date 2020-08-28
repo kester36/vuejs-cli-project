8b426171dd142f5d4da45cd87c23962baf8280df
Petro Shpyhotskyi <shpyhotskyi@gmail.com>
2020-08-28 17:17:10 +0300

    Revert test module
    This reverts commit 8765e311c86b11aa5a74f512114826e60a44bcce.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Learn from './learn'
 
 Vue.config.productionTip = false
-
-console.log(`Variable i ${Learn.i} and a ${Learn.a}`);
 
 new Vue({
   render: h => h(App),
