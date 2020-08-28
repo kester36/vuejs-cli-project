8765e311c86b11aa5a74f512114826e60a44bcce
Petro Shpyhotskyi <shpyhotskyi@gmail.com>
2020-08-28 17:16:43 +0300

    create module

---
+++ src/learn.js
@@ -0,0 +1,5 @@
+// export let i = 10;
+let i = 10;
+let a = 22;
+
+export default {i, a};
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Learn from './learn'
 
 Vue.config.productionTip = false
+
+console.log(`Variable i ${Learn.i} and a ${Learn.a}`);
 
 new Vue({
   render: h => h(App),
